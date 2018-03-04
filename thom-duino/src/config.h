28cd4a65a4e0217f31c7856428a5e3f08c8d9cbf
Robert Roth <robert.roth.off@gmail.com>
2018-03-04 04:17:50 +0200

    Move defines to a separate header

---
+++ thom-duino/src/config.h
@@ -0,0 +1,47 @@
+#include <menu.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+#include <menuIO/adafruitGfxOut.h>
+
+//PCD8544 aka nokia5110
+#define GFX_DC  5
+#define GFX_CS  4
+#define GFX_RST 10
+#define BACKLIGHTPIN 7
+
+#define CONTRAST 60
+
+// rotary encoder pins
+#define encA    8
+#define encB    9
+#define encBtn  3
+
+// the back button
+#define escBtn  2
+
+#define DEBOUNCE_DELAY 300
+
+// define menu colors --------------------------------------------------------
+//  {{disabled normal,disabled selected},{enabled normal,enabled selected, enabled editing}}
+//monochromatic color table
+const colorDef<uint16_t> colors[] MEMMODE={
+  {{WHITE,BLACK},{WHITE,BLACK,BLACK}},//bgColor
+  {{BLACK,WHITE},{BLACK,WHITE,WHITE}},//fgColor
+  {{BLACK,WHITE},{BLACK,WHITE,WHITE}},//valColor
+  {{BLACK,WHITE},{BLACK,WHITE,WHITE}},//unitColor
+  {{BLACK,WHITE},{WHITE,WHITE,WHITE}},//cursorColor
+  {{BLACK,WHITE},{WHITE,BLACK,BLACK}},//titleColor
+};
+
+#define gfxWidth 84
+#define gfxHeight 48
+#define fontX 6
+//5
+#define fontY 9
+
+// the modes to be supported
+#define FAVOURITE_RADIO 0
+#define VIDSTREAM 1
+#define MUSIC 2
+
+#define BOOLEAN_TOGGLE(name,variable,label) result name() {Serial.print(label); Serial.println(variable?"on":"off");return proceed;}
--- thom-duino/src/main.cpp
+++ thom-duino/src/main.cpp
@@ -10,9 +10,6 @@ input: Encoder with switch + Serial + switch
 ***/
 
 #include <SPI.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_PCD8544.h>
-#include <menu.h>
 #include <menuIO/encoderIn.h>
 #include <menuIO/keyIn.h>
 #include <menuIO/chainStream.h>
@@ -22,40 +19,15 @@ input: Encoder with switch + Serial + switch
 #include <ClickEncoder.h>
 #include <TimerOne.h>
 
+#include "config.h"
 
 using namespace Menu;
 
-//PCD8544 aka nokia5110
-#define GFX_DC  5
-#define GFX_CS  4
-#define GFX_RST 10
-
 Adafruit_PCD8544 gfx(GFX_DC,GFX_CS,GFX_RST);
 
-#define BACKLIGHTPIN 7
-#define CONTRAST 60
 
-// rotary encoder pins
-#define encA    8
-#define encB    9
-#define encBtn  3
-
-// the back button
-#define escBtn  2
-
-// the modes to be supported
-#define FAVOURITE_RADIO 0
-#define VIDSTREAM 1
-#define MUSIC 2
-
-// buttons software debounce delay
-#define DEBOUNCE_DELAY 300
-#define BOOLEAN_TOGGLE(name,variable,label) result name() {Serial.print(label); Serial.println(variable?"on":"off");return proceed;}
 // time of last button press for debounce
-
 long lastPress = 0;
-
-
 
 // status variables
 int volume = 60, oldVolume = volume;
@@ -158,25 +130,6 @@ MENU(mainMenu,"Main menu",doNothing,noEvent,wrapStyle
   ,SUBMENU(setMode)
   ,EXIT("<Back")
 );
-
-// define menu colors --------------------------------------------------------
-//  {{disabled normal,disabled selected},{enabled normal,enabled selected, enabled editing}}
-//monochromatic color table
-const colorDef<uint16_t> colors[] MEMMODE={
-  {{WHITE,BLACK},{WHITE,BLACK,BLACK}},//bgColor
-  {{BLACK,WHITE},{BLACK,WHITE,WHITE}},//fgColor
-  {{BLACK,WHITE},{BLACK,WHITE,WHITE}},//valColor
-  {{BLACK,WHITE},{BLACK,WHITE,WHITE}},//unitColor
-  {{BLACK,WHITE},{WHITE,WHITE,WHITE}},//cursorColor
-  {{BLACK,WHITE},{WHITE,BLACK,BLACK}},//titleColor
-};
-
-#define gfxWidth 84
-#define gfxHeight 48
-#define fontX 6
-//5
-#define fontY 9
-#define MAX_DEPTH 3
 
 encoderIn<encA,encB> encoder;//simple quad encoder driver
 encoderInStream<encA,encB> encStream(encoder,4);// simple quad encoder fake Stream
